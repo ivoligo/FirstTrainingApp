ae0057caadbec371e2479e334bf0655be8cf8da0
OLIvanov <OLIvanov@systematic.ru>
2021-08-11 11:26:46 +0300

    added app files;

---
+++ src/main/java/ru/systematic/firstTrainingApp/FirstTrainingApplication.java
@@ -0,0 +1,13 @@
+package ru.systematic.firstTrainingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/systematic/firstTrainingApp/FirstTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.systematic.firstTrainingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
